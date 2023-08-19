67b0acdc9f7babaf321f186adf6e2428b7c5c7f4
annmonrajijames <annmonjameschiramel@gmail.com>
2023-08-19 10:54:12 +0530

    tried to clear errors in @angular imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { StreamComponent } from './pages/stream/stream.component';
+import { Bluetooth1Component } from './pages/bluetooth1/bluetooth1.component';
 //... other imports
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'stream', component: StreamComponent },
+  { path: 'bluetooth1', component: Bluetooth1Component },
   //... other routes
 ];
 
--- src/app/pages/bluetooth1/bluetooth1.component.ts
+++ src/app/pages/bluetooth1/bluetooth1.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+// bluetooth1.component.ts
+
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BleClient } from '@capacitor-community/bluetooth-le';
 
 @Component({
   selector: 'app-bluetooth1',
   templateUrl: './bluetooth1.component.html',
-  styleUrls: ['./bluetooth1.component.scss'],
+  styleUrls: ['./bluetooth1.component.scss']
 })
-export class Bluetooth1Component  implements OnInit {
+export class Bluetooth1Component {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {}
+  async handleBluetooth() {
+    try {
+      await BleClient.initialize();
+
+      const targetDevice = await BleClient.requestDevice();
+      console.log(JSON.stringify(targetDevice)); 
+
+      if (targetDevice && targetDevice.name === 'Ganesh A53') {
+        await BleClient.connect(targetDevice.deviceId);
+        alert('Connected to ' + targetDevice.deviceId);
+      } else {
+        alert('No device selected or matching the filter');
+      }
+
+    } catch (error) {
+      console.error('Bluetooth Error:', error);
+      alert('Bluetooth Error: ' + error);
+    }
+    this.router.navigate(['/stream']);
+  }
 
 }
